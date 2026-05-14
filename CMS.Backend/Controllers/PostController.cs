92d3511ac057197b61b3ae6e8bc75ef5a21d2a1b
Ndlam0211 <nguyendinhlam0211@gmail.com>
2026-05-14 16:00:34 +0700

    Add PostController and post list view with sample data

---
+++ CMS.Backend/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using CMS.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Backend.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Tạo danh sách dữ liệu mẫu trực tiếp trong code
+            var list = new List<Post> {
+                new Post { Id = 1, Title = "Lộ trình học ASP.NET cho người mới"},
+                new Post { Id = 2, Title = "Cài đặt ReactJS"},
+                new Post { Id = 3, Title = "Hướng dẫn cài đặt Visual Studio"}
+            };
+
+            return View(list); // Gửi danh sách này sang giao diện
+        }
+    }
+}

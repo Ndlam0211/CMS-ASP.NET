a1a932b79952380663880ceeb4f337e6731c2a13
Ndlam0211 <nguyendinhlam0211@gmail.com>
2026-05-28 15:08:01 +0700

    Update dashboard

--- CMS.Backend/Controllers/HomeController.cs
+++ CMS.Backend/Controllers/HomeController.cs
@@ -25,6 +25,24 @@ namespace CMS.Backend.Controllers
                     .Take(3) // Chỉ lấy đúng 3 bản tin đầu tiên
                     .ToList();
 
+            // Lấy tổng số lượng từ các bảng
+            var totalPosts = _context.Posts.Count();
+            var totalCategories = _context.Categories.Count();
+            var totalUsers = _context.Users.Count();
+            var totalCustomers = _context.Customers.Count();
+            var totalOrders = _context.Orders.Count();
+            var totalProducts = _context.Products.Count();
+            var totalCategoryProducts = _context.CategoriesProducts.Count();
+
+            // Gửi dữ liệu qua ViewBag
+            ViewBag.TotalPosts = totalPosts;
+            ViewBag.TotalCategories = totalCategories;
+            ViewBag.TotalUsers = totalUsers;
+            ViewBag.TotalCustomers = totalCustomers;
+            ViewBag.TotalOrders = totalOrders;
+            ViewBag.TotalProducts = totalProducts;
+            ViewBag.TotalCategoryProducts = totalCategoryProducts;
+
             return View(latestPosts);
         }
     }

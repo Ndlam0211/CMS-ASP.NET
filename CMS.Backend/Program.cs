5a90316f33096670e638f9127d15a9ef8c242d3b
Ndlam0211 <nguyendinhlam0211@gmail.com>
2026-05-28 16:17:22 +0700

    Config app for using swagger and Restful API

--- CMS.Backend/Program.cs
+++ CMS.Backend/Program.cs
@@ -4,8 +4,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ==============================================================
+// 1. KHU VỰC ĐĂNG KÝ DỊCH VỤ (SERVICES CONTAINER)
+// ==============================================================
+
 // Add services to the container.
+//Lệnh này vừa nhận diện các API mới, vừa giữ quyền biên dịch các View (.cshtml) của Web MVC cũ.
 builder.Services.AddControllersWithViews();
+
+// Đăng ký dịch vụ lõi giúp hệ thống tự động bóc tách thông tin Endpoint phục vụ Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); // Kích hoạt bộ sinh tài liệu API Swagger
 
 // Đăng ký DbContext vào hệ thống
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -20,6 +29,17 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 
 var app = builder.Build();
 
+// ==============================================================
+//  2. KHU VỰC CẤU HÌNH MIDDLEWARE (REQUEST PIPELINE)
+// ==============================================================
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "LamCMS Web API v1");
+    c.RoutePrefix = "swagger"; // -- Đường dẫn truy cập mặc định sẽ là /swagger
+});
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -33,9 +53,21 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// [VỊ TRÍ ĐẶT CORS]: Phải nằm ngay giữa UseRouting và app.UseAuthentication(); UseAuthorization();
+app.UseCors("AllowAll");
+
 app.UseAuthentication();
 app.UseAuthorization();
 
+// ===============================================================
+// 3. KHU VỰC ĐỊNH TUYẾN PHÂN LUỒNG (ROUTING MAP)
+// ===============================================================
+
+// Phân luồng A: 
+// Ánh xạ các Endpoint API tuân thủ theo cấu trúc [Route("api/[controller]")]
+app.MapControllers();
+
+// Phân luồng B: Giữ lại bản đồ đường đi mặc định cho trang giao diện Web MVC cũ
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
